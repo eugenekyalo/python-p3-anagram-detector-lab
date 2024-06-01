1227e0e31e6e13c0f813860a8cb0f9b74d697f76
Eugene Kyalo <yovlinpentdragonvii@gmail.com>
2024-06-01 11:16:22 +0300

    Implemented Anagram Detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        matches = []
+        for possible_anagram in possible_anagrams:
+            if self.is_anagram(possible_anagram):
+                matches.append(possible_anagram)
+        return matches
+
+    def is_anagram(self, word):
+        return sorted(word.lower()) == sorted(self.word)
